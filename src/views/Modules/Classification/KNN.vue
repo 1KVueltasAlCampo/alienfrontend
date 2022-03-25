1edd1aee5b49fbf51abfa4a59db9d1313c8270d6
Juan David Cruz Garcia <71021752+1KVueltasAlCampo@users.noreply.github.com>
2022-03-25 09:59:05 -0500

    Css updated, button added
    Css updated, button addded.

--- src/views/Modules/Classification/KNN.vue
+++ src/views/Modules/Classification/KNN.vue
@@ -1,22 +1,22 @@
 <template>
-  <div id="wrapper">
-    <section id="inputSection">
+  <div class ="wrapper">
+    <section class = "wrapper" id="inputSection">
       <section id="titleSection">
-        <label>Variables'</label>
+        <label>Variables</label>
         <label>values</label>
       </section>
-      <section class="inputSection">
+      <section class="inputs">
         <label>X</label>
-        <input type="text" class="textInput" id="" />
+        <input type="text" class="textInput" />
       </section>
 
-      <section class="inputSection">
+      <section class="inputs">
         <label>Y</label>
-        <input type="text" class="textInput" id="" />
+        <input type="text" class="textInput" />
       </section>
     </section>
     <section id="outputSection">
-      <label>Charts</label>
+      <input type="button" class="ml-5 btn btn-primary expand" value="Create charts"/>
     </section>
   </div>
 </template>
@@ -26,8 +26,14 @@
 </script>
 
 <style scoped>
+
 #titleSection label {
   margin-right: 23%;
+}
+
+.inputs{
+  display: flex;
+  gap: 25%;
 }
 
 .inputSection label {
@@ -39,5 +45,18 @@
   grid-template-columns: 1fr 1fr;
 }
 
+.inputSection{
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+#outputSection{
+ grid-column: 1fr;
+}
+
+.textInput{
+  display: grid;
+  grid-template-columns: 1fr;
+}
 
 </style>
